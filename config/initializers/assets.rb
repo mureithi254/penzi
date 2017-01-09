d0a1be893f85122389648c44bfd24bf0fd4a5cec
mureithi254 <lnewhenio@gmail.com>
2017-01-10 00:38:23 +0300

    initial commit-unresponsive header

---
+++ config/initializers/assets.rb
@@ -0,0 +1,21 @@
+# Be sure to restart your server when you modify this file.
+
+# Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.assets.version = '1.0'
+
+# Add additional assets to the asset load path
+Rails.application.config.assets.precompile += %w( responsiveslides.min.js )
+Rails.application.config.assets.precompile += %w( hover_pack.js )
+Rails.application.config.assets.precompile += %w( easing.js )
+Rails.application.config.assets.precompile += %w( move-top.js )
+Rails.application.config.assets.precompile += %w( wow.min.js )
+
+Rails.application.config.assets.precompile += %w( animate.css )
+Rails.application.config.assets.precompile += %w( style.css )
+Rails.application.config.assets.precompile += %w( hover_pack.css )
+Rails.application.config.assets.precompile += %w( component.css )
+# Rails.application.config.assets.paths << Emoji.images_path
+
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+# Rails.application.config.assets.precompile += %w( search.js )
---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  get 'news/news'
+
+  get 'gallery/gallery'
+
+  get 'contact/contact'
+
+  get 'about/about'
+
+  get 'home/index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
