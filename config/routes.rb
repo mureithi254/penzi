6ee2e0072fe3f94f60ede4e11b70bffedd44facc
mureithi254 <lnewhenio@gmail.com>
2017-01-10 23:07:40 +0300

    fully functional basic app

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   get 'about/about'
 
-  get 'home/index'
+  root 'home#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
